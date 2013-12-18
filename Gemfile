b8cfe3320b155091b3dfc9539ff3fbc94a0df685
Chris <chris.haack@me.com>
2013-12-18 16:43:50 -0500

    add files

---
+++ Gemfile
@@ -0,0 +1,12 @@
+source "https://rubygems.org"
+
+gem "sinatra"
+gem "activerecord"
+gem "sinatra-activerecord"
+gem "rake"
+
+group :development do
+	gem "debugger"
+  gem "shotgun"
+  gem "tux"
+end
---
+++ app.rb
@@ -0,0 +1,23 @@
+require 'bundler'
+Bundler.require
+
+Dir.glob('./lib/*.rb') do |model|
+  require model
+end
+
+module Name
+  class App < Sinatra::Application
+    
+    #configure
+    configure do
+      set :root, File.dirname(__FILE__)
+      set :public_folder, 'public'
+    end
+
+    #routes
+    get '/' do
+      erb :index
+    end
+
+  end
+end
